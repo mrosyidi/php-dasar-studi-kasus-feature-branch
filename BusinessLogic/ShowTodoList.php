4820b325fea23e14b2f4cde00e31fdd6b5d9e17f
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-25 21:32:44 +0700

    update ShowTodoList.php menampilkan todolist

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -1,5 +1,10 @@
 <?php
   function showTodoList()
   {
+    global $todoList;
 
+    echo "TODOLIST" . PHP_EOL;
+    foreach($todoList as $number => $value){
+      echo "$number. $value" . PHP_EOL;
+    }
   }

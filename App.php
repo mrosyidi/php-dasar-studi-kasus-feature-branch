b3ce0f67960894e8fb8dd04c443476891dd5192a
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-25 13:27:09 +0700

    update App.php require ViewShowTodoList.php

--- App.php
+++ App.php
@@ -3,5 +3,6 @@
   require_once __DIR__ . "/BusinessLogic/ShowTodoList.php";
   require_once __DIR__ . "/BusinessLogic/AddTodoList.php";
   require_once __DIR__ . "/BusinessLogic/RemoveTodoList.php";
+  require_once __DIR__ . "/View/ViewShowTodoList.php";
 
   echo "Aplikasi Todolist" . PHP_EOL;

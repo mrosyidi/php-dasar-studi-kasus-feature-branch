a969ca98953f67115db0f0233b34e19bc6ee696b
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-26 08:00:42 +0700

    add TestViewShowTodoList.php

---
+++ Test/TestViewShowTodoList.php
@@ -0,0 +1,11 @@
+<?php
+  require_once "../View/ViewShowTodoList.php";
+
+  $todoList[1] = "Eko";
+  $todoList[2] = "Kurniawan";
+  $todoList[3] = "Khannedy";
+  $todoList[4] = "Programmer";
+  $todoList[5] = "Zaman";
+  $todoList[6] = "Now";
+
+  viewShowTodoList();

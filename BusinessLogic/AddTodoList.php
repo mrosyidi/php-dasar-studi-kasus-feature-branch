3622c12ae1ef9dc42d3fc0de27b39c3a584baa72
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-26 14:17:07 +0700

    update AddTodoList.php menambah todolist

--- BusinessLogic/AddTodoList.php
+++ BusinessLogic/AddTodoList.php
@@ -1,5 +1,9 @@
 <?php
-  function addTodoList()
+  function addTodoList(string $todo)
   {
+    global $todoList;
 
+    $number = is_array($todoList) ? sizeof($todoList) : 0;
+    $number = $number + 1;
+    $todoList[$number] = $todo;
   }
